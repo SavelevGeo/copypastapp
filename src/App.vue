73ff3271188560913d6c781a75ce5dbe967a8005
Georgii Savelev <savelevgeo.post@gmail.com>
2026-01-08 00:20:38 +0300

    create project object with localized attributes

--- src/App.vue
+++ src/App.vue
@@ -11,16 +11,18 @@ Retrieved 2026-01-03, License - CC BY-SA 4.0
             <header class="border-bottom">
                 <div class="container text-center py-3">
                     <a href="/" class="text-dark text-decoration-none">
-                        <span>Пополнить подорожник</span>
+                        <span>{{ currentProject.name[currentLocale] }}</span>
                     </a>
                 </div>
             </header>
-            <a href="#" class="btn mt-5" role="button" @click.prevent="copyToClipboard"
-                :data-text="'96433078361620599568105003'"><i class="bi bi-copy me-2"></i>Гошин</a>
-            <a href="#" class="btn mt-5" role="button" @click.prevent="copyToClipboard"
-                :data-text="'96433078361289155632263724'"><i class="bi bi-copy me-2"></i>Ксюшенькин</a>
-            <a href="http://sberbank.com/sms/shpa/?cs=668341289737&psh=f&did=1760948078147000853" class="btn mt-5"
-                role="button"><i class="bi bi-box-arrow-up-right"></i> в Сбер</a>
+            <template v-for="projectItem in currentProject.items">
+                <a v-if="projectItem.copy" href="#" class="btn mt-5" role="button" @click.prevent="copyToClipboard"
+                    :data-text="projectItem.copyText[currentLocale]"><i class="bi bi-copy me-2"></i>{{
+                        projectItem.displayText[currentLocale] }}</a>
+                <a v-if="projectItem.redirect" :href="projectItem.copyText[currentLocale]" class="btn mt-5"
+                    role="button"><i class="bi bi-box-arrow-up-right"></i> {{ projectItem.displayText[currentLocale]
+                    }}</a>
+            </template>
         </div>
 
         <div class="toast-container position-fixed top-0 end-0 p-3">
@@ -32,8 +34,69 @@ Retrieved 2026-01-03, License - CC BY-SA 4.0
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { Ref, ref } from 'vue';
 import { BApp, BToast } from 'bootstrap-vue-next';
+
+type Localized<T> = Record<string, T>;
+interface ProjectItem {
+    order: Localized<number>;
+    displayText: Localized<string>;
+    copyText: Localized<string>;
+    copy: boolean;
+    redirect: boolean;
+}
+interface Project {
+    name: Localized<string>;
+    items: ProjectItem[];
+}
+
+const currentLocale: Ref<string> = ref('ru');
+const currentProject: Ref<Project> = ref({
+    name: {
+        'ru': 'Пополнить подорожник',
+    },
+    items: [
+        {
+            order: {
+                'ru': 1
+            },
+            displayText: {
+                'ru': 'Гошин',
+            },
+            copyText: {
+                'ru': '96433078361620599568105003'
+            },
+            copy: true,
+            redirect: false
+        },
+        {
+            order: {
+                'ru': 2
+            },
+            displayText: {
+                'ru': 'Ксюшенькин',
+            },
+            copyText: {
+                'ru': '96433078361289155632263724'
+            },
+            copy: true,
+            redirect: false
+        },
+        {
+            order: {
+                'ru': 3
+            },
+            displayText: {
+                'ru': 'В Сбер',
+            },
+            copyText: {
+                'ru': 'http://sberbank.com/sms/shpa/?cs=668341289737&psh=f&did=1760948078147000853'
+            },
+            copy: false,
+            redirect: true
+        }
+    ]
+});
 
 const showToast = ref(false)
 const toastTitle = ref('')
